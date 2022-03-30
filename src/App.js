87d7492631e3cb3f53ff936bbab00fdee8a10bba
AR456789 <94092324+AR456789@users.noreply.github.com>
2022-03-29 20:00:29 -0700

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      
+      <div className="container"> 
+      <footer>
+        <a 
+        href="https://github.com/AR456789/react-homework-1"
+        target="_blank"
+        > This project was coded by Adelaide and is open-sourced </a>
+      </footer>
+      </div>
     </div>
   );
 }
