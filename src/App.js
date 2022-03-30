111c54385a389bb155193b29b3f2efdf4f469efc
AR456789 <94092324+AR456789@users.noreply.github.com>
2022-03-29 21:15:53 -0700

    Added a Weather App h1

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container"> 
+      <h1> Weather App </h1>
       <footer>
         <a 
         href="https://github.com/AR456789/react-homework-1"
