4b6b51ec1a750c45d340856808cf0f51bd626283
AR456789 <94092324+AR456789@users.noreply.github.com>
2022-03-29 21:52:25 -0700

    Added HTML

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-
+import Weather from "./Weather";
 export default function App() {
   return (
     <div className="App">
       <div className="container"> 
-      <h1> Weather App </h1>
+      <Weather />
       <footer>
         <a 
         href="https://github.com/AR456789/react-homework-1"
