3514476efff6dc60516e807e03547b0c7fe245cd
AR456789 <94092324+AR456789@users.noreply.github.com>
2022-03-30 17:37:10 -0700

    Added css

--- src/Weather.js
+++ src/Weather.js
@@ -7,10 +7,10 @@ export default function Weather() {
             <form>
             <div className="row">
             <div className="col-9">
-                <input type="search" placeholder="Enter a city" className="search-bar"/>
+                <input type="search" placeholder="Enter a city" className="search-bar " autoFocus="on"/>
             </div>
             <div className="col-3">
-                <input type="submit" value="Search" className="btn btn-primary"/>
+                <input type="submit" value="Search" className="btn btn-primary w-100"/>
             </div>
             </div>
             </form>
@@ -19,13 +19,16 @@ export default function Weather() {
                 <li> Wednesday 7:00</li>
                 <li> Mostly Cloudy</li>
             </ul>
-             <div className="row">
+             <div className="row mt-3">
                  <div className="col-6">
+                     <div className="clearfix">
                      <img
                         src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
                         alt="Mostly Cloudy"
                      />
-                     6°C
+                     <span className="temperature"> 6 </span>
+                     <span className="unit"> °C </span>
+                     </div>
                  </div>
                  <div className="col-6">
                      <ul>
