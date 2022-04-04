d220a43d8652ee271254cb4de68a09ca4a86ef1a
AR456789 <94092324+AR456789@users.noreply.github.com>
2022-04-03 21:23:01 -0500

    Added weather icons

---
+++ src/WeatherIcon.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactAnimatedWeather from "react-animated-weather";
+
+export default function WeatherIcon(props) {
+   console.log(props.code);
+   const codeMapping = {
+    "clear sky": "CLEAR_DAY",
+    "scattered clouds": "PARTLY_CLOUDY_DAY",
+    "few clouds": "PARTLY_CLOUDY_DAY",
+    "overcast clouds": "CLOUDY",
+    "broken clouds": "CLOUDY",
+    "shower rain": "RAIN",
+    "rain": "RAIN",
+    "thunderstorm": "RAIN",
+    "snow": "SNOW",
+    "mist": "FOG",
+   };
+   
+    return (
+    <ReactAnimatedWeather
+    icon={codeMapping[props.code]}
+    color="#1e1e1e"
+    size={64}
+    animate={true}
+  />
+    );
+}
