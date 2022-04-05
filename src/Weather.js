dbe809c7bddd44f9e1f7bb6842ae827bef3e8666
AR456789 <94092324+AR456789@users.noreply.github.com>
2022-04-04 19:29:35 -0500

    changed api url

--- src/Weather.js
+++ src/Weather.js
@@ -22,7 +22,7 @@ export default function Weather(props) {
 
     function search() {
         const apiKey = "333d1e7c5f22cf63ff65c69aa26537e4";
-    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse)
     }
     function handleSubmit(event) {
