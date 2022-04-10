cb9bf36addc18411e54e9d69502428113dfbb4b8
AR456789 <94092324+AR456789@users.noreply.github.com>
2022-04-10 17:07:21 -0500

    css changes

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -14,12 +14,30 @@ export default function WeatherIcon(props) {
     "thunderstorm": "RAIN",
     "snow": "SNOW",
     "mist": "FOG",
+    "01d": "CLEAR_DAY",
+    "01n": "CLEAR_DAY",
+    "02d": "PARTLY_CLOUDY_DAY",
+    "02n": "PARTLY_CLOUDY_DAY",
+    "03d": "PARTLY_CLOUDY_DAY",
+    "03n": "PARTLY_CLOUDY_DAY",
+    "04d": "CLOUDY",
+    "04n": "CLOUDY",
+    "09d": "RAIN",
+    "09n": "RAIN",
+    "10d": "RAIN",
+    "10n": "RAIN",
+    "11d": "RAIN",
+    "11n": "RAIN",
+    "13d": "SNOW",
+    "13n": "SNOW",
+    "50d": "FOG",
+    "50n": "FOG",
    };
    
     return (
     <ReactAnimatedWeather
     icon={codeMapping[props.code]}
-    color="#1e1e1e"
+    color="lavender"
     size={props.size}
     animate={true}
   />
